666ed850c19c9e6f1ae94946fdcbbf0465e26f78
wustxing <dev04@odun.com>
2017-08-23 19:34:45 +0800

    1

--- src/Component/common/template.jsx
+++ src/Component/common/template.jsx
@@ -47,7 +47,10 @@ const Main = mySeting => {
     }
 
     //mapStateToProps and mapDispatchToProps
-    return connect(state => { //将顶层组件与模版绑定后return回去，配置路由的时候用的就是和redux绑定的组件，所以其实每个路由匹配的都是同一个组件，只不过这个组件的内容不同
+    return connect(state => { 
+    //将顶层组件与模版绑定后return回去，
+    //配置路由的时候用的就是和redux绑定的组件，
+    //所以其实每个路由匹配的都是同一个组件，只不过这个组件的内容不同
         let {producRecord, saleRecord,requestData, testData} = state;
         return { 
             state: state['fetchData'],
